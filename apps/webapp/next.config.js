2a63163734a0c50679466553977a5bdc804b99d3
Ivan Kashlyuk <savills.expert@gmail.com>
2025-10-27 20:05:02 +0300

    fix(webapp): add webpack alias to next.config.js

--- apps/webapp/next.config.js
+++ apps/webapp/next.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,6 +9,13 @@ const nextConfig = {
   env: {
     API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:3001',
     WEBAPP_BASE_URL: process.env.WEBAPP_BASE_URL || 'http://localhost:3000',
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
   },
 };
 
